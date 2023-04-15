1299484d23b914d2e9026b6abed11f7e83f96a93
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2023-04-15 11:33:28 +0200

    NPW-2 Added Typing letters inside hero - typed.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 const burger = document.querySelector(".nav__burger");
-
 burger.addEventListener("click", () => burger.classList.toggle("active"));
-
 window.addEventListener("resize", () => {
   if (window.innerWidth > 991) {
     burger.classList.remove("active");
   }
+});
+
+const typedArea = document.querySelector(".hero__container__typed");
+const typedTitles = typedArea.dataset.typed.split(".");
+new Typed(".hero__container__typed", {
+  strings: typedTitles,
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 2000,
 });
