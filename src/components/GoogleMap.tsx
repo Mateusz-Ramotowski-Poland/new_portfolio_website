6a5b42e6a1ff662a6e6a30a5ee1b29fb8d14325f
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2023-07-03 21:14:31 +0200

    MAIN Map responsive 300px - 1920px

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -4,7 +4,7 @@ export const GoogleMap = () => {
   return (
     <iframe
       className={styles.iframe}
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d604618.0094913378!2d21.307863858109485!3d53.704203772545405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e2289e1252ca45%3A0xbe60ce7248418c42!2sKamionki%2025%2C%2011-500%20Kamionki!5e0!3m2!1spl!2spl!4v1686164500739!5m2!1spl!2spl"
+      src="https://maps.google.com/maps?q=gdańsk&t=&z=5&ie=UTF8&iwloc=&output=embed"
       width="600"
       height="450"
       allowFullScreen={true}
